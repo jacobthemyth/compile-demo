cd1e37e628068a3bf3abd5d56e90529016d07c47
Jacob Smith <jacob@jacobsmith.io>
2016-09-30 10:29:52 -0400

    Add Playground#html
    - `rails g migration AddHtmlToPlaygrounds html:string`
    - `RAILS_ENV=test rails db:migrate`
    
    Fixes:
    
    ```
    1) Playground should have db column named html
         Failure/Error: it { is_expected.to have_db_column(:html) }
           Expected Playground to have db column named html (Playground does not have a db column named html.)
         # ./spec/models/playground_spec.rb:4:in `block (2 levels) in <top (required)>'
    
      2) Creating a new Playground as a visitor
         Failure/Error: <%= f.input :html %>
    
         ActionView::Template::Error:
           undefined method `html' for #<Playground:0x007fc731146810>
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930141040) do
+ActiveRecord::Schema.define(version: 20160930142859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160930141040) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "html"
     t.index ["user_id"], name: "index_playgrounds_on_user_id", using: :btree
   end
 
