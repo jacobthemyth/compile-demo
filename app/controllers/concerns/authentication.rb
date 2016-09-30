9aaa8df70131ca61fa6bba9f3686e2a819baa42f
Jacob Smith <jacob@jacobsmith.io>
2016-09-30 09:38:27 -0400

    Finish implementing Authentication#current_user
    Fixes:
    
    ```
    ActiveRecord::StatementInvalid: PG::UndefinedTable: ERROR:  relation "users" does not exist
    ```
    
    This is a weird one, but basically it's because I'm integrating with
    ActiveRecord, and it was calling `create`, which was actually hitting
    the DB.

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -1,7 +1,11 @@
 module Authentication
   def current_user
-    token = User::Token.new
-    session[:remember_token] = token
-    Visitor.create(remember_token: token)
+    if session[:remember_token].present?
+      Visitor.find_by!(remember_token: session[:remember_token])
+    else
+      token = User::Token.new
+      session[:remember_token] = token
+      Visitor.create(remember_token: token)
+    end
   end
 end
