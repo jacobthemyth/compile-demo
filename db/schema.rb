e9cf4454cef645b0861102e7adc5ea1b10ace2c6
Jacob Smith <jacob@jacobsmith.io>
2016-09-30 10:40:06 -0400

    Implement Playground#js
    Fixes:
    
    ```
    1) Creating a new Playground as a visitor
         Failure/Error: <%= f.input :js %>
    
         ActionView::Template::Error:
           undefined method `js' for #<Playground:0x007fdff9363ba8>
         # ./app/views/playgrounds/new.html.erb:4:in `block in _app_views_playgrounds_new_html_erb___4204673798256425772_70299967475560'
         # ./app/views/playgrounds/new.html.erb:1:in `_app_views_playgrounds_new_html_erb___4204673798256425772_70299967475560'
         # ./spec/features/creating_a_new_playground_spec.rb:5:in `block (2 levels) in <top (required)>'
         # ------------------
         # --- Caused by: ---
         # NoMethodError:
         #   undefined method `js' for #<Playground:0x007fdff9363ba8>
         #   ./app/views/playgrounds/new.html.erb:4:in `block in _app_views_playgrounds_new_html_erb___4204673798256425772_70299967475560'
    
      2) Playground should have db column named js
         Failure/Error: it { is_expected.to have_db_column(:js) }
           Expected Playground to have db column named js (Playground does not have a db column named js.)
         # ./spec/models/playground_spec.rb:6:in `block (2 levels) in <top (required)>'
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930143453) do
+ActiveRecord::Schema.define(version: 20160930143741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160930143453) do
     t.integer  "user_id"
     t.string   "html"
     t.string   "css"
+    t.string   "js"
     t.index ["user_id"], name: "index_playgrounds_on_user_id", using: :btree
   end
 
