b306bd7c55a840a8a1f26ffdd95ae6cbd060b454
Jacob Smith <jacob@jacobsmith.io>
2016-09-30 09:58:12 -0400

    Create users table
    - `rails g migration CreateUsers`
    - `RAILS_ENV=test rails db:migrate`
    
    Fixes:
    
    ```
    ActiveRecord::StatementInvalid: PG:: UndefinedTable: ERROR:  relation "users" does not exist
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928183038) do
+ActiveRecord::Schema.define(version: 20160930135346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20160928183038) do
   create_table "playgrounds", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
   end
 
 end
