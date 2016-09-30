c22eb08f1164baf7b2a8c6af90a76b363425c1f9
Jacob Smith <jacob@jacobsmith.io>
2016-09-30 10:26:35 -0400

    Add remember_token to users
    - `rails g migration AddRememberTokenToUsers remember_token:string`
    - `RAILS_ENV=test rails db:migrate`
    
    Fixes:
    
    ```
    Expected User to have db column named remember_token (User does not have a db column named remember_token.)
    ActiveModel::UnknownAttributeError: unknown attribute 'remember_token' for Visitor
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930135346) do
+ActiveRecord::Schema.define(version: 20160930135906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160930135346) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "remember_token"
   end
 
 end
