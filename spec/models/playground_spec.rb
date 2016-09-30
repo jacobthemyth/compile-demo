ab99c67bc3cbc4ea01ccbe167cbd5f0bbd69682a
Jacob Smith <jacob@jacobsmith.io>
2016-09-30 10:42:46 -0400

    Write failing spec for Playground#css

--- spec/models/playground_spec.rb
+++ spec/models/playground_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Playground, type: :model do
   it { is_expected.to have_db_column(:html) }
+  it { is_expected.to have_db_column(:css) }
   it { is_expected.to belong_to(:user) }
 end
