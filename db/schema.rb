2652e7c58199924b73a2b2bdd2297c89ac0ecafb
Jacob Smith <jacob@jacobsmith.io>
2016-09-30 10:35:28 -0400

    Implement Playground#css
    - `rails g migration AddCssToPlaygrounds css:string`
    - `RAILS_ENV=test rails db:migrate`
    
    Fixes:
    ```
    1) Creating a new Playground as a visitor
         Failure/Error: <%= f.input :css %>
    
         ActionView::Template::Error:
           undefined method `css' for #<Playground:0x007fdff9363ba8>
         # ./app/views/playgrounds/new.html.erb:3:in `block in _app_views_playgrounds_new_html_erb___4204673798256425772_70299967475560'
         # ./app/views/playgrounds/new.html.erb:1:in `_app_views_playgrounds_new_html_erb___4204673798256425772_70299967475560'
         # ./spec/features/creating_a_new_playground_spec.rb:5:in `block (2 levels) in <top (required)>'
         # ------------------
         # --- Caused by: ---
         # NoMethodError:
         #   undefined method `css' for #<Playground:0x007fdff9363ba8>
         #   ./app/views/playgrounds/new.html.erb:3:in `block in _app_views_playgrounds_new_html_erb___4204673798256425772_70299967475560'
    
      2) Playground should have db column named css
         Failure/Error: it { is_expected.to have_db_column(:css) }
           Expected Playground to have db column named css (Playground does not have a db column named css.)
         # ./spec/models/playground_spec.rb:5:in `block (2 levels) in <top (required)>'
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930142859) do
+ActiveRecord::Schema.define(version: 20160930143453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160930142859) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.string   "html"
+    t.string   "css"
     t.index ["user_id"], name: "index_playgrounds_on_user_id", using: :btree
   end
 
