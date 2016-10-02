203fcb4b4da864950e8af33b253a19eb7e932a01
Jacob Smith <jacob@jacobsmith.io>
2016-10-02 15:52:27 -0400

    Implement html_document_string

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
-  def html_document_string
+  def html_document_string(content = {})
+      (<<~EOS).gsub(/^\s+$\n/, '')
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset=utf-8>
+            #{content[:head]}
+          </head>
+          <body>
+            #{content[:body]}
+          </body>
+        </html>
+      EOS
   end
 end
