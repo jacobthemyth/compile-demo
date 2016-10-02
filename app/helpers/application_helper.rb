e3895dcc84960b30bea019dd4d39f70d8bbcf801
Jacob Smith <jacob@jacobsmith.io>
2016-10-02 16:15:17 -0400

    Implement data_uri_for_playground

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,11 @@ module ApplicationHelper
   end
 
   def data_uri_for_playground(playground)
-    "data:text/html;charset=utf-8,"
+    head_content = ""
+    head_content << "<style>#{playground.css}</style>" if playground.css.present?
+    head_content << "<script>#{playground.js}</script>" if playground.js.present?
+    doc = html_document_string(head: head_content, body: playground.html)
+    encoded_html = URI.encode(doc)
+    "data:text/html;charset=utf-8,#{encoded_html}"
   end
 end
